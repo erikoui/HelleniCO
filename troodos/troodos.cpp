af41d5e320b9f7bcf586439dea4ef073455a3bf4
Erik <erik@debian.temp>
2017-08-18 22:53:06 +0300

    No progress

--- troodos/troodos.cpp
+++ troodos/troodos.cpp
@@ -52,7 +52,7 @@ int main(int argc, char **argv)
     uint32_t i,j;
     uint32_t input_from,input_to,input_weight;
     uint32_t output_sub1,output_sub2,output_sub3;//max distance, sum(max-min)for each node, 3rd subproblem
-    uint32_t maxdist_to_current=0;
+    uint32_t maxdist_to_current=0,index_of_current,index_of_previous,mindist_of_previous,mindist_to_current,maxdist_of_previous,weight_of_j_to_i;
     
     edge edge_buffer;
     vector<node> graph,tpsortedgraph;
@@ -88,12 +88,12 @@ int main(int argc, char **argv)
     //find max distance from 0 to n-1
     for(i=1;i<tpsortedgraph.size();i++){//for all nodes in topological order
         //current maxdist is max(in[j].maxdist+weight)
-        int index_of_current=tpsortedgraph[i].index;
+        index_of_current=tpsortedgraph[i].index;
         maxdist_to_current=0;
         for(j=0;j<graph[index_of_current].in.size();j++){//for all nodes that lead to node i
-            int index_of_previous=graph[index_of_current].in[j].to;
-            int maxdist_of_previous=graph[index_of_previous].maxdist;
-            int weight_of_j_to_i= graph[index_of_current].in[j].weight;
+            index_of_previous=graph[index_of_current].in[j].to;
+            maxdist_of_previous=graph[index_of_previous].maxdist;
+            weight_of_j_to_i= graph[index_of_current].in[j].weight;
             if(weight_of_j_to_i+maxdist_of_previous>maxdist_to_current){
                 maxdist_to_current=weight_of_j_to_i+maxdist_of_previous;
             }
@@ -101,8 +101,36 @@ int main(int argc, char **argv)
         graph[tpsortedgraph[i].index].maxdist=maxdist_to_current;
     }
     output_sub1=graph[graph.size()-1].maxdist;//save answer to subproblem 1
-        
-    //TODO: sub2 is get sum of max(0 to i)-min(0 to i) for i=0 to n-1
+
+    //TODO: sub2 is NOT get sum of max(0 to i)-min(0 to i) for i=0 to n-1
+    //find min distance to all nodes
+    for(i=1;i<tpsortedgraph.size();i++){//for all nodes in topological order
+        //current mindist is max(in[j].mindist+weight)
+        index_of_current=tpsortedgraph[i].index;
+        mindist_to_current=999999;
+        for(j=0;j<graph[index_of_current].in.size();j++){//for all nodes that lead to node i
+            index_of_previous=graph[index_of_current].in[j].to;
+            mindist_of_previous=graph[index_of_previous].mindist;
+            weight_of_j_to_i= graph[index_of_current].in[j].weight;
+            if(weight_of_j_to_i+mindist_of_previous<mindist_to_current){
+                mindist_to_current=weight_of_j_to_i+mindist_of_previous;
+            }
+        }
+        graph[tpsortedgraph[i].index].mindist=mindist_to_current;
+    }
+    output_sub2=0;
+    for(i=0;i<graph.size();i++){
+        output_sub2+=graph[i].maxdist-graph[i].mindist;
+    }
+//        traverse the graph as if i am the fire cars
+//    queue<int> q;holds stations where i need to go
+//    q.push(0);
+//    while(q!=empty){while there are still stations to go to
+//        for(i=0;i<graph[q.front()].out.size();i++){
+//            q.push(graph[q.front()].out[i].to);
+//        }
+//        
+//        q.pop();
     //TODO: sub3 is something weird which i have not thought about yet
  
     fin.close();
